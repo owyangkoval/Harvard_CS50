711ec1ad4bb641890913fbe8aae7a346c98881c6
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 23:41:30 +0000

    Mon, Jan 10, 2022, 11:41 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -29,8 +29,24 @@ for (int i = 0; i < n; i++)
     {
         printf("#");
     }
+
+    // here I have two for loops nested inside the above for loop,
+    // I previously made the mistake of having two inner loops nested.
+
+    // this 2nd for loop prints n-1-i spaces
+    // because if n=5, then in the 4th row, there will be 5-1-3 spaces/dots
+    for (int j = 0; j < n + 1 - i; j++)
+    {
+        printf(" ");
+    }
+    // this 3rd for loop prints i+1 hashes
+    // because if n=5, then in the 4th row, there will be 3+1 hashes.
+    // (3 because you count from 0)
+    for (int j = 0; j < i - 1; j++)
+    {
+        printf("#");
+    }
     printf("\n");
-  }
 
-
- }
+}
+}
