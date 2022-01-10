af48b328228c98f784558ff0c467994c62861b64
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 22:40:59 +0000

    Mon, Jan 10, 2022, 10:40 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,27 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int n;
-    do
-    {
-        n = get_int("Size: ");
-    }
-    while (n < 1) ;
+int main(void) {
+    //initialize variables
+    int height, n, j, k, i;
 
+    printf("Height: \n");
+    // Get user input
+    height = Get_Int();
+    n = height;
+    for(i = 0; i < height; i++) {
+        // create n spaces based off height
+        for(k = n; k > i; k--)
+            printf("%c", ' ');
 
-    // For each row
-    for (int i = 0; i < n - 1; i++)
-    {
-        // For each column
-        for (int j = 0; j < n + 2; j++)
-        {
-            // Print a pyramid
+        // create hash tags
+        for(j = 0; j < i+2; j++)
             printf("#");
-        }
 
-        // Move to next row
         printf("\n");
     }
+    return 0;
 }
