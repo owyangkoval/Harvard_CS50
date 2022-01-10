8c5d47ca95b15b1bdaa2880e63e4d71b1843946b
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 22:13:12 +0000

    Mon, Jan 10, 2022, 10:13 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,23 +1,18 @@
-#include<stdio.h>
-int main() {
-   int r, s, rows=0;
-   int t=0;
-   clrscr();
-   printf("Enter number of rows to print the pyramid: ");
-   scanf("%d", &rows);
-   printf("\n");
-   printf("The Pyramid Pattern for the number of rows are:");
-   printf("\n\n");
-   for(r=1;r<=rows;++r,t=0) {
-      for(s=1; s<=rows-r; ++s){
-         printf(" ");
-      }
-      while (t!=2*r-1) {
-         printf("* ");
-         ++t;
-      }
-      printf("\n");
-   }
-   getch();
-   return 0;
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
 }
