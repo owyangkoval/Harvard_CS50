f786463f0bffe7b0ba09a01c43900279aa52fa84
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 22:51:33 +0000

    Mon, Jan 10, 2022, 10:51 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,26 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 9);
 
-    //initialize variables
-    int height, n, j, k, i;
-    printf("Height: \n");
-
-    // Get user input
-    height = get_int("Size: ");
-    n = height;
-    for(i = 0; i < height; i++) {
-
-    // create n spaces based off height
-        for(k = n; k > i; k--)
-            printf("%c", ' ');
-
-        // create hash tags
-        for(j = 0; j < i+2; j++)
+    // For each row
+    for (int i = 0; i <= n; i++)
+    {
+        // For each column
+        for (int j = 0; j <= n; j++)
+        {
+            // Print a brick
             printf("#");
+        }
 
+        // Move to next row
         printf("\n");
     }
-    return 0;
 }
