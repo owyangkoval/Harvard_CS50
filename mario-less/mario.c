0cec8121aacf134ca679b6644aa54bfd3f3ef0e1
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 23:48:00 +0000

    Mon, Jan 10, 2022, 11:48 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,33 +1,38 @@
+#include <cs50.h>
 #include <stdio.h>
+
 
 int main(void)
 {
     int n;
 do
 {
-    n = get_int("Height: ");
+    n = get_int("Size: ");
 }
 while (n < 1 || n > 8);
+// this for loop makes new lines
+
+for (int i = 0; i < n; i++)
 {
-    int row,col,space,n1=4, n2=4;
-
-    for(row=1;row<=n1;row++){
-
-    for(space=n2-row;space>=0;space--)
+    for (int j = 0; j < n - 1 - i; j++)
+    {
         printf(" ");
-
-    for(col=1;col<=row;col++)
-        printf("*");
-
-    printf("##");
-
-    for(col=1;col<=row;col++)
-        printf("*");
-
-    for(space=n2-row;space>=0;space--)
-        printf(" ");
-
     }
 
+    for (int j = 0; j < i + 1; j++)
+    {
+        printf("#");
+    }
+
+    for (int j = 0; j > n - 1 - i; j++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < i + 1; j++)
+    {
+        printf("#");
+    }
     printf("\n");
+
+}
 }
