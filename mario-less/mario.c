64916d80fc7d9f7505f31831931ebe10a76f46fa
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 23:38:36 +0000

    Mon, Jan 10, 2022, 11:38 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,28 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
-string hash(int);
 
 int main(void)
 {
     int n;
-    do
-    {
-        n = get_int("Height: ");
-    }
-    while (n < 0 || n > 8);
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n-(1-i); j++)
-        {
-            for(int j = 0; j < i+1; j++)
-            {
-                printf(".");
-            }
-            printf("#");
-        }
-        printf("\n");
-    }
-
-
+do
+{
+    n = get_int("Height: ");
 }
+while (n < 1 || n > 8);
+// this for loop makes new lines
+for (int i = 0; i < n; i++)
+{
+    // here I have two for loops nested inside the above for loop,
+    // I previously made the mistake of having two inner loops nested.
+
+    // this 2nd for loop prints n-1-i spaces
+    // because if n=5, then in the 4th row, there will be 5-1-3 spaces/dots
+    for (int j = 0; j < n - 1 - i; j++)
+    {
+        printf(" ");
+    }
+    // this 3rd for loop prints i+1 hashes
+    // because if n=5, then in the 4th row, there will be 3+1 hashes.
+    // (3 because you count from 0)
+    for (int j = 0; j < i + 1; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+  }
+
+
+ }
