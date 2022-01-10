62ed19f3621d732b99d4f54db1e14b134dd26305
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-10 22:00:37 +0000

    Mon, Jan 10, 2022, 10:00 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,26 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
 {
-    int n;
-    do
-    {
-        n = get_int("Size: ");
-    }
-    while (n < 1);
-
     // For each row
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 8; i++)
     {
-        // For each column
-        for (int j = 0; j < n; j++)
-        {
-            // Print a brick
-            printf("#");
-        }
+        printf("#");
+    }
+    printf("\n");
 
-        // Move to next row
-        printf("\n");
+        // For each column
+    for (int i = 0; i < 8; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
     }
 }
