3b468b94760c21da84aceed43df4490d41e67092
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-12 23:59:02 +0000

    Wed, Jan 12, 2022, 11:59 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,8 +8,28 @@ int main(void)
      long Number = get_long("Card Number: ");
 
      if
-     // number of digits in card
+     {
+     // number of digits AMEX
      ( Number /= 15)
-
      printf("AMEX\n");
+
+
+     else
+     {
+     // number of digits MASTERCARD
+     ( Number /= 16);
+     printf("MASTERCARD\n");
+     }
+
+     else
+     {
+     // number of digits VISA
+     ( Number /= 13 || Number /=16 )
+     printf("VISA\n");
+     }
+
+     else
+     // number of digits INVALID
+     printf("INVALID\n");
+
 }
