fb3ee2e7b740b8a102c71dcf3c2aa29131dba226
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 22:31:49 +0000

    Thu, Jan 13, 2022, 10:31 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,21 +2,33 @@
 #include <stdio.h>
 #include <math.h>
 
+int main(void);
 
-int main(void)
-{
-     long num = get_long("Card Number: ");
-     if ( num > 12 && num < 17 )
-}
-          // digits
-          while ()
-          {
-               int dig1 = num % 10
-               int num2 = (num-(num % 10))/10
-               int dig2 = num2 % 10
-               int num3 = (num2-(num2 % 10))/10
-               int dig3 = num3 % 10
-          }
+     num = get_long("Card Number: ")
 
+    for (num < 16 && num > 12);
+     do 
      {
-     }
+                int dig2 = ((num % 100)/10)*2;
+                int dig4 = ((num % 10000)/1000)*2;
+                int dig6 = ((num % 1000000)/100000)*2;
+                int dig8 = ((num % 100000000)/1000000)*2;
+                int dig10 = ((num % 10000000000)/100000000)*2;
+                int dig12 = ((num % 1000000000000)/10000000000)*2;
+                int dig14 = ((num % 100000000000000)/1000000000000)*2;
+                int dig16 = ((num % 10000000000000000)/100000000000000)*2;
+
+                dig2 = ((dig2 % 10) + (dig2 % 100)/10);
+                dig4 = ((dig4 % 10) + (dig4 % 100)/10);
+                dig6 = ((dig6 % 10) + (dig6 % 100)/10);
+                dig8 = ((dig8 % 10) + (dig8 % 100)/10);
+                dig10 = ((dig10 % 10) + (dig10 % 100)/10);
+                dig12 = ((dig12 % 10) + (dig12 % 100)/10);
+                dig14 = ((dig14 % 10) + (dig14 % 100)/10);
+                dig16 = ((dig16 % 10) + (dig16 % 100)/10);
+
+    int sumodd = dig2 + dig4 + dig6+ dig8 + dig10 + dig12 + dig14 + dig16;
+}
+{
+    printf("sumodd: %i\n," sumodd)
+}
--- pseudocode.c
+++ pseudocode.c
@@ -1,36 +1,27 @@
+
+
+
+
+
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
 
+
 int main(void)
-
-    do
 {
-     long num = get_long("Card Number: ")
+     long num = get_long("Card Number: ");
+     if ( num > 12 && num < 17 )
 }
-    while (num < 16 && num > 12);
+          // digits
+          while ()
+          {
+               int dig1 = num % 10
+               int num2 = (num-(num % 10))/10
+               int dig2 = num2 % 10
+               int num3 = (num2-(num2 % 10))/10
+               int dig3 = num3 % 10
+          }
 
-                int dig2 = ((num % 100)/10)*2;
-                int dig4 = ((num % 10000)/1000)*2;
-                int dig6 = ((num % 1000000)/100000)*2;
-                int dig8 = ((num % 100000000)/1000000)*2;
-                int dig10 = ((num % 10000000000)/100000000)*2;
-                int dig12 = ((num % 1000000000000)/10000000000)*2;
-                int dig14 = ((num % 100000000000000)/1000000000000)*2;
-                int dig16 = ((num % 10000000000000000)/100000000000000)*2;
-
-                dig2 = ((dig2 % 10) + (dig2 % 100)/10);
-                dig4 = ((dig4 % 10) + (dig4 % 100)/10);
-                dig6 = ((dig6 % 10) + (dig6 % 100)/10);
-                dig8 = ((dig8 % 10) + (dig8 % 100)/10);
-                dig10 = ((dig10 % 10) + (dig10 % 100)/10);
-                dig12 = ((dig12 % 10) + (dig12 % 100)/10);
-                dig14 = ((dig14 % 10) + (dig14 % 100)/10);
-                dig16 = ((dig16 % 10) + (dig16 % 100)/10);
-
-    int sumodd = dig2 + dig4 + dig6+ dig8 + dig10 + dig12 + dig14 + dig16;
-}
-{
-    printf("sumodd: %i\n," sumodd)
-}
-
+     {
+     }
