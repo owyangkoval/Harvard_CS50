47e44de0a3908ff4e38e5c1e6767753f353e316c
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 23:44:14 +0000

    Thu, Jan 13, 2022, 11:44 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,16 +5,16 @@
 int main(void)
 
      {
-     long number = get_long("Card Number: ");
+     num = get_long("Card Number: ");
      }
-                int dig2 = ((number % 100)/10)*2;
-                int dig4 = ((number % 10000)/1000)*2;
-                int dig6 = ((number % 1000000)/100000)*2;
-                int dig8 = ((number % 100000000)/1000000)*2;
-                int dig10 = ((number % 10000000000)/100000000)*2;
-                int dig12 = ((number % 1000000000000)/10000000000)*2;
-                int dig14 = ((number % 100000000000000)/1000000000000)*2;
-                int dig16 = ((number  % 10000000000000000)/100000000000000)*2;
+                int dig2 = ((num % 100)/10)*2;
+                int dig4 = ((num % 10000)/1000)*2;
+                int dig6 = ((num % 1000000)/100000)*2;
+                int dig8 = ((num % 100000000)/1000000)*2;
+                int dig10 = ((num % 10000000000)/100000000)*2;
+                int dig12 = ((num % 1000000000000)/10000000000)*2;
+                int dig14 = ((num % 100000000000000)/1000000000000)*2;
+                int dig16 = ((num  % 10000000000000000)/100000000000000)*2;
 
                 int dig2_1 = ((dig2 % 10) + (dig2 % 100)/10);
                 int dig4_1 = ((dig4 % 10) + (dig4 % 100)/10);
@@ -25,14 +25,14 @@ int main(void)
                 int dig14_1  = ((dig14 % 10) + (dig14 % 100)/10);
                 int dig16_1 = ((dig16 % 10) + (dig16 % 100)/10);
 
-                int dig1 = (number % 10);
-                int dig3 = (number % 1000)
-                int dig5 = (number % 100000)
-                int dig7 = (number % 10000000)
-                int dig9 = (number % 1000000000);
-                int dig11 = (number % 100000000000);
-                int dig13 = (number % 10000000000000);
-                int dig15 = (number % 1000000000000000);
+                int dig1 = (num % 10);
+                int dig3 = (num % 1000)
+                int dig5 = (num % 100000)
+                int dig7 = (num % 10000000)
+                int dig9 = (num % 1000000000);
+                int dig11 = (num % 100000000000);
+                int dig13 = (num % 10000000000000);
+                int dig15 = (num % 1000000000000000);
 
 
     int sumeven = dig2_1 + dig4_1 + dig6_1+ dig8_1 + dig10_1 + dig12_1 + dig14_1 + dig16_1;
@@ -41,11 +41,16 @@ int main(void)
     int finalsum = sumeven + sumodd
 }
 
-     while (long < 16 || long > 14)
+     if (num < 16 || num > 14)
           {
-               dig15 == 3, dig14 ==4, dig14 ==
+               dig15 == 3, dig14 == 4;
           }
-
+     else {
+               dig15 == 3 && dig14 == 7;
+          }
+     {
+          printf("AMEX/n");
+     }
 
 }
 {
