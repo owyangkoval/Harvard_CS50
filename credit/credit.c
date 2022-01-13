89e27d74433b90d64544f6e49ebf6e30be3eeb19
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 21:34:18 +0000

    Thu, Jan 13, 2022, 9:34 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,17 +5,17 @@
 
 int main(void)
 {
-     int dig;
      long num = get_long("Card Number: ");
 }
           // digits
-          while (dig=0; dig<16; dig++);
+          while (num=0; num<16; num++);
+
           {
                int dig = num % 10
                int num2 = (num-(num % 10))/10
 
 
-               
+
                int dig2 = num2 % 10
                int num3 = (num2-(num2 % 10))/10
                int dig3 = num3 % 10
