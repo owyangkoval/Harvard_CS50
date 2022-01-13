6be2adb877cb1a8af5a1e3f141dfc96efabd8326
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 22:12:37 +0000

    Thu, Jan 13, 2022, 10:12 PM +00:00

--- pseudocode.c
+++ pseudocode.c
@@ -7,14 +7,18 @@ int main(void)
 {
      long num = get_long("Card Number: ");
 }
-if
+        while (num < 16 && num > 12)
+            {
+                int dig2 = ((num % 100)/10)*2
+                int dig4 = ((num % 10000)/1000)*2
+                int dig6 = ((num % 1000000)/100000)*2
+                int dig8 = ((num % 100000000)/1000000)*2
+                int dig10 = ((num % 10000000000)/100000000)*2
+                int dig12 = ((num % 1000000000000)/10000000000)*2
+                int dig14 = ((num % 100000000000000)/1000000000000)*2
+                int dig14 = ((num % 10000000000000000)/100000000000000)*2
+            }
 {
-    int dig2 = ((num % 10)/10)
-    int dig4 = num
-    int dig6
-
-}
-{
-    printf()
+    printf("dig2: %i\n," dig2)
 }
 
