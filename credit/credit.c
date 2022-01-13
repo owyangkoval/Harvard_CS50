0ad6230cff63db604094153023a87226f2b9fe1d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 01:06:46 +0000

    Thu, Jan 13, 2022, 1:06 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,7 +7,9 @@ int main(void)
      long Number = get_long("Card Number: ");
      int SecondDigit = (Number % 100/10);
      int FourthDigit = (Number % 100/10);
-
+     int SixthDigit = (Number % 100/10);
+     int EigthDigit = (Number % 100/10);
+     int TenthDigit = (Number % 100/10);
 
 
      printf("SecondDigit: %i\n", SecondDigit);
