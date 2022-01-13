9bb22de760ecb0527f69f1a0fde3a6f0df09ab11
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 22:54:08 +0000

    Thu, Jan 13, 2022, 10:54 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,10 +4,11 @@
 
 int main(void);
 
-     num = get_long("Card Number: ")
+     long num = get_long("Card Number: ");
 
-    while (num < 16 && num > 12);
+    do(num < 16 && num > 12)
 
+     while
      {
                 int dig2 = ((num % 100)/10)*2;
                 int dig4 = ((num % 10000)/1000)*2;
@@ -18,19 +19,19 @@ int main(void);
                 int dig14 = ((num % 100000000000000)/1000000000000)*2;
                 int dig16 = ((num % 10000000000000000)/100000000000000)*2;
 
-                dig2 = ((dig2 % 10) + (dig2 % 100)/10);
-                dig4 = ((dig4 % 10) + (dig4 % 100)/10);
-                dig6 = ((dig6 % 10) + (dig6 % 100)/10);
-                dig8 = ((dig8 % 10) + (dig8 % 100)/10);
-                dig10 = ((dig10 % 10) + (dig10 % 100)/10);
-                dig12 = ((dig12 % 10) + (dig12 % 100)/10);
-                dig14 = ((dig14 % 10) + (dig14 % 100)/10);
-                dig16 = ((dig16 % 10) + (dig16 % 100)/10);
+                dig2_1 = ((dig2 % 10) + (dig2 % 100)/10);
+                dig4_1 = ((dig4 % 10) + (dig4 % 100)/10);
+                dig6_1  = ((dig6 % 10) + (dig6 % 100)/10);
+                dig8_1  = ((dig8 % 10) + (dig8 % 100)/10);
+                dig10_1  = ((dig10 % 10) + (dig10 % 100)/10);
+                dig12_1  = ((dig12 % 10) + (dig12 % 100)/10);
+                dig14_1  = ((dig14 % 10) + (dig14 % 100)/10);
+                dig16_1 = ((dig16 % 10) + (dig16 % 100)/10);
 
-    int sumeven = dig2 + dig4 + dig6+ dig8 + dig10 + dig12 + dig14 + dig16;
+    int sumeven = dig2_1 + dig4_1 + dig6_1+ dig8_1 + dig10_1 + dig12_1 + dig14_1 + dig16_1;
     int sumodd = dig1 + dig3 + dig5 + dig7 + dig9 + dig11 + dig13 + dig15;
 
-    sumeven + sumodd = 
+    sumeven + sumodd =
 }
 {
     printf("sumodd: %i\n," sumodd)
