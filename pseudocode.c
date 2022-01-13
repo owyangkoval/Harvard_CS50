746c8752de103f618aeb7f9ffd9e8d77844f6e10
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-13 22:29:42 +0000

    Thu, Jan 13, 2022, 10:29 PM +00:00

--- pseudocode.c
+++ pseudocode.c
@@ -2,24 +2,35 @@
 #include <stdio.h>
 #include <math.h>
 
-
 int main(void)
+
+    do
 {
-     long num = get_long("Card Number: ");
+     long num = get_long("Card Number: ")
 }
-        while (num < 16 && num > 12)
-            {
-                int dig2 = ((num % 100)/10)*2
-                int dig4 = ((num % 10000)/1000)*2
-                int dig6 = ((num % 1000000)/100000)*2
-                int dig8 = ((num % 100000000)/1000000)*2
-                int dig10 = ((num % 10000000000)/100000000)*2
-                int dig12 = ((num % 1000000000000)/10000000000)*2
-                int dig14 = ((num % 100000000000000)/1000000000000)*2
-                int dig14 = ((num % 10000000000000000)/100000000000000)*2
-            }
-                dig2 = 
+    while (num < 16 && num > 12);
+
+                int dig2 = ((num % 100)/10)*2;
+                int dig4 = ((num % 10000)/1000)*2;
+                int dig6 = ((num % 1000000)/100000)*2;
+                int dig8 = ((num % 100000000)/1000000)*2;
+                int dig10 = ((num % 10000000000)/100000000)*2;
+                int dig12 = ((num % 1000000000000)/10000000000)*2;
+                int dig14 = ((num % 100000000000000)/1000000000000)*2;
+                int dig16 = ((num % 10000000000000000)/100000000000000)*2;
+
+                dig2 = ((dig2 % 10) + (dig2 % 100)/10);
+                dig4 = ((dig4 % 10) + (dig4 % 100)/10);
+                dig6 = ((dig6 % 10) + (dig6 % 100)/10);
+                dig8 = ((dig8 % 10) + (dig8 % 100)/10);
+                dig10 = ((dig10 % 10) + (dig10 % 100)/10);
+                dig12 = ((dig12 % 10) + (dig12 % 100)/10);
+                dig14 = ((dig14 % 10) + (dig14 % 100)/10);
+                dig16 = ((dig16 % 10) + (dig16 % 100)/10);
+
+    int sumodd = dig2 + dig4 + dig6+ dig8 + dig10 + dig12 + dig14 + dig16;
+}
 {
-    printf("dig2: %i\n," dig2)
+    printf("sumodd: %i\n," sumodd)
 }
 
