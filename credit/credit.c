d3208fa7f2f0ee389c47133b22bf5f31b085f1cf
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-14 19:18:06 +0000

    Fri, Jan 14, 2022, 7:18 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -64,16 +64,16 @@ int main(void)
                }
 
      //AMEX
-          else if (dig15 == 3 && (dig14 == 4 || dig14 == 7) && lastdigit == 0)
-               {
-               printf("AMEX\n");
-               }
+               else if (dig15 == 3 && (dig14 == 4 || dig14 == 7) && lastdigit == 0)
+                    {
+                    printf("AMEX\n");
+                    }
 
      //VISA
-          else if ((dig13 == 4 && lastdigit == 0) || (dig14 == 4 && lastdigit == 0))
-               {
-               printf("VISA\n");
-               }
+               else if ((dig13 == 4 && lastdigit == 0) || (dig14 == 4 && lastdigit == 0))
+                    {
+                    printf("VISA\n");
+                    }
 
      //INVALID
           else
