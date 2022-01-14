c63a007554733fb4709e29b6d0191a766ce2ad1d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-14 00:15:37 +0000

    Fri, Jan 14, 2022, 12:15 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,7 +5,7 @@
 int main(void)
 
 {
-     long num;
+     long num, dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8, dig9, dig10, dig11, dig12, dig13, dig14, dig15, dig16, dig2_1, dig4_1, dig6_1, dig8_1, dig10_1, dig12_1, dig14_1, dig16_1, sumeven, sumodd, finalsum;
      do
     {
      num = get_long("Card Number: ");
@@ -15,38 +15,38 @@ int main(void)
 
 
      {
-                int dig2 = ((num % 100)/10)*2;
-                int dig4 = ((num % 10000)/1000)*2;
-                int dig6 = ((num % 1000000)/100000)*2;
-                int dig8 = ((num % 100000000)/1000000)*2;
-                int dig10 = ((num % 10000000000)/100000000)*2;
-                int dig12 = ((num % 1000000000000)/10000000000)*2;
-                int dig14 = ((num % 100000000000000)/1000000000000)*2;
-                int dig16 = ((num  % 10000000000000000)/100000000000000)*2;
+                dig2 = ((num % 100)/10)*2;
+                dig4 = ((num % 10000)/1000)*2;
+                dig6 = ((num % 1000000)/100000)*2;
+                dig8 = ((num % 100000000)/1000000)*2;
+                dig10 = ((num % 10000000000)/100000000)*2;
+                dig12 = ((num % 1000000000000)/10000000000)*2;
+                dig14 = ((num % 100000000000000)/1000000000000)*2;
+                dig16 = ((num  % 10000000000000000)/100000000000000)*2;
 
-                int dig2_1 = ((dig2 % 10) + (dig2 % 100)/10);
-                int dig4_1 = ((dig4 % 10) + (dig4 % 100)/10);
-                int dig6_1  = ((dig6 % 10) + (dig6 % 100)/10);
-                int dig8_1  = ((dig8 % 10) + (dig8 % 100)/10);
-                int dig10_1  = ((dig10 % 10) + (dig10 % 100)/10);
-                int dig12_1  = ((dig12 % 10) + (dig12 % 100)/10);
-                int dig14_1  = ((dig14 % 10) + (dig14 % 100)/10);
-                int dig16_1 = ((dig16 % 10) + (dig16 % 100)/10);
+                dig2_1 = ((dig2 % 10) + (dig2 % 100)/10);
+                dig4_1 = ((dig4 % 10) + (dig4 % 100)/10);
+                dig6_1  = ((dig6 % 10) + (dig6 % 100)/10);
+                dig8_1  = ((dig8 % 10) + (dig8 % 100)/10);
+                dig10_1  = ((dig10 % 10) + (dig10 % 100)/10);
+                dig12_1  = ((dig12 % 10) + (dig12 % 100)/10);
+                dig14_1  = ((dig14 % 10) + (dig14 % 100)/10);
+                dig16_1 = ((dig16 % 10) + (dig16 % 100)/10);
 
-                int dig1 = (num % 10);
-                int dig3 = (num % 1000);
-                int dig5 = (num % 100000);
-                int dig7 = (num % 10000000);
-                int dig9 = (num % 1000000000);
-                int dig11 = (num % 100000000000);
-                int dig13 = (num % 10000000000000);
-                int dig15 = (num % 1000000000000000);
+                dig1 = (num % 10);
+                dig3 = (num % 1000);
+                dig5 = (num % 100000);
+                dig7 = (num % 10000000);
+                dig9 = (num % 1000000000);
+                dig11 = (num % 100000000000);
+                dig13 = (num % 10000000000000);
+                dig15 = (num % 1000000000000000);
 
 
-    int sumeven = dig2_1 + dig4_1 + dig6_1+ dig8_1 + dig10_1 + dig12_1 + dig14_1 + dig16_1;
-    int sumodd = dig1 + dig3 + dig5 + dig7 + dig9 + dig11 + dig13 + dig15;
+    sumeven = dig2_1 + dig4_1 + dig6_1+ dig8_1 + dig10_1 + dig12_1 + dig14_1 + dig16_1;
+    sumodd = dig1 + dig3 + dig5 + dig7 + dig9 + dig11 + dig13 + dig15;
 
-    int finalsum = sumeven + sumodd;
+    finalsum = sumeven + sumodd;
      }
 
 {
