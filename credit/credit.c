ff512697019deca0aa872f041b3fa2fbab65e59d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-14 00:20:47 +0000

    Fri, Jan 14, 2022, 12:20 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,7 +5,7 @@
 int main(void)
 
 {
-     long num, dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8, dig9, dig10, dig11, dig12, dig13, dig14, dig15, dig16, dig2_1, dig4_1, dig6_1, dig8_1, dig10_1, dig12_1, dig14_1, dig16_1, sumeven, sumodd, finalsum;
+     long num, int dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8, dig9, dig10, dig11, dig12, dig13, dig14, dig15, dig16, dig2_1, dig4_1, dig6_1, dig8_1, dig10_1, dig12_1, dig14_1, dig16_1, sumeven, sumodd, finalsum, lastdigit;
      do
     {
      num = get_long("Card Number: ");
@@ -46,9 +46,10 @@ int main(void)
     sumodd = dig1 + dig3 + dig5 + dig7 + dig9 + dig11 + dig13 + dig15;
 
     finalsum = sumeven + sumodd;
+     lastdigit = (finalsum % 10);
      }
 {
-     if ( = )
+     if (lastdigit= )
 }
 
 {
