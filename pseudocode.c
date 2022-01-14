deadf74eb869ba25cfa165a40ac5e0c6ce4ea5ba
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-14 18:45:28 +0000

    Fri, Jan 14, 2022, 6:45 PM +00:00

--- pseudocode.c
+++ pseudocode.c
@@ -24,3 +24,29 @@
      }
 
 }
+
+     //MASTERCARD
+     else if(num == 16 && dig16 == 5 && (dig14 == 5 || dig14 == 1) && (lastdigit == 0))
+     {
+          printf("MASTERCARD\n");
+     }
+
+     // AMEX
+     if (dig15 == 3 && (dig14 == 4 || dig14 == 7) && lastdigit == 0)
+     {
+          printf("AMEX\n");
+     }
+
+     //VISA
+     else if ((dig13 == 4 && lastdigit == 0) || (dig14 == 4 && lastdigit == 0))
+     {
+          printf("VISA\n");
+     }
+
+     //INVALID
+     else
+     {
+          printf("INVALID\n");
+     }
+
+}
