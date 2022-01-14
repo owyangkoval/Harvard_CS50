3c598724414691c19ce6cc86de048277cd982985
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-14 23:15:01 +0000

    Fri, Jan 14, 2022, 11:15 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,32 +1,16 @@
 #include <cs50.h>
-#include <stdio.h>
-
-
 int main(void)
+
 {
-    int height, row, column, space;
-    do
-    {
-        height = get_int ("Size: ");
-    }
-    while (height < 1 || height > 8);
+    // Here your code !
+    int x, y;
 
-    for (row = 0; row < height; row++)
-    {
-        for (space = 0; space < height - row - 1; space++)
+    if (x == y)
         {
-            printf(" ");
+            return (x + y)*3;
         }
-        for (column = 0; column <= row; column++)
+    else (x > y || x < y)
         {
-            printf("#");
+            return (x + y);
         }
-        printf("  ");
-                for (column = 0; column <= row; column++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
-    }
 }
