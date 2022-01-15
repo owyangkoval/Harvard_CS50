1eb209b00b434c9133a50d53de7695a78f0bf75a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-15 01:25:49 +0000

    Sat, Jan 15, 2022, 1:25 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void);
+int main(void)
 
 {
     int i = get_int("Size: ");
     int row, column, space;
 
     //row
-    for (row = 0; row < n-1; row++)
+    for (row = 0; row < i; row++)
     {
-        printf(" ");
+        printf(" \n");
     }
         //column
         for (column = 0; column < i; column++)
         {
-            printf("#");
+            printf("#\n");
         }
 }
