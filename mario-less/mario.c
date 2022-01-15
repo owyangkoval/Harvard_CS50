08f2dd73efdda716f7ab012ab1565a34c6117d4a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-15 01:19:41 +0000

    Sat, Jan 15, 2022, 1:19 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,12 +7,14 @@ int main(void);
     int i = get_int("Size: ");
     int row, column, space;
 
+    //row
     for (row = 0; row < 9; row++)
     {
         printf("#");
     }
-     for (column = 0; column < 9; column++)
-    {
-        printf(" ");
-    }
+        //column
+        for (column = 0; column < 9; column++)
+        {
+            printf(" ");
+        }
 }
