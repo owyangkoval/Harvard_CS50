5aa3e83e92aa9b72b06dd0ec5aae78b0e3999e00
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-19 00:29:05 +0000

    Wed, Jan 19, 2022, 12:29 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,11 +10,15 @@ int compute_score(string word);
 
 int main(void)
 {
-    int scores 
+    int letters [26]
+
+        Letter [A, E, I, L, N, O, R, S, T, U] = 1
+
 }
     // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
+
 
     // Score both words
     int score1 = compute_score(word1);
