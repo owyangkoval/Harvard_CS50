0aee23c89b8fc7b504a75e874981bfdf484f0fed
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-19 01:23:10 +0000

    Wed, Jan 19, 2022, 1:23 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,19 +8,9 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 int compute_score(string word);
 
+
 int main(void)
 {
-    char points[87];
-
-
-        points[1] = {A, E, I, L, N, O, R, S, T, U}
-        letters[D, G) = 2
-        points[3] = (B, C, M, D)
-        points[4] = ([F, H, V, W, Y)
-        points[5] = K
-        points[8] = (J, X)
-        points[10] = Q, Z)
-
     // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
