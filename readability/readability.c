17e931bb35ae2c5eb395ee0211f87bb86760e732
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-19 01:54:04 +0000

    Wed, Jan 19, 2022, 1:54 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,18 +7,14 @@ int main(void)
 
 {
     string statement = get_string("Statement: ");
-    int grade, l, w, s;
-    int n = strlen(statement);
-    int x = 0.0588 * L - 0.296 * S - 15.8
+    int grade, letters, words, sentence, letter_sum, sentence_sum;
 
-printf("Grade %i", grade);
+    letter_sum = (letters/words)*100;
+    sentence_sum = (sentence/words)*100;
 
-    for (l = 0, n = strlen(statement); l < n; l++)
+    grade = 0.0588 * letter_sum - 0.296 * sentence_sum - 15.8;
+
     {
-            for (w = 0; w < l; w++)
-            {
-                for (s = 0, s < w; s++)
-            }
+        printf("Grade %i", grade);
     }
-    printf("\n");
 }
