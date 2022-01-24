a07c021d2f87292759c2d440c081b9051355158c
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 22:31:30 +0000

    Mon, Jan 24, 2022, 10:31 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,9 +6,9 @@
 int main(int argc, string argv[]){
 
     string plaintext = get_string("Plaintext: ");
-    int key = get_int("Key: ");
+    int i = get_int("Key: ");
 
-    int i, p, c;
+    int p, c;
     char k[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
