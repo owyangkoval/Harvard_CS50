4c10c6a6d7c9a58764c594081d0fe040d0fa4cbe
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 20:42:50 +0000

    Mon, Jan 24, 2022, 8:42 PM +00:00

---
+++ caesar/caesar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(int argc, string argv[]){
+
+    int k;
+    char c;
+    char p[51];
+
+        p[0]='a';
+        p[1]='b';
+        p[2]='c';
+        p[2]='d';
+
+}
--- readability/readability.c
+++ readability/readability.c
@@ -1,13 +1,1 @@
-#include <cs50.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
 
-
-int main() {
-
-    string text = get_string("Text: ");
-    printf("%s\n," text);
-    int count_letters(string text);
-
-}
