d9dd0341d29ae34cb91f2eda7a30b365911b68e2
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 20:53:42 +0000

    Mon, Jan 24, 2022, 8:53 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,6 +7,6 @@ int main(int argc, string argv[]){
 string p = get_string("Plaintext: ");
     int k;
     char c;
-    char letters[51] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
+    char letters[51] = {'a',b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
 
     }
