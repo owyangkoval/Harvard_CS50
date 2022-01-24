031d84f574f47031ae24121a8318ebc418818f8a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 22:28:56 +0000

    Mon, Jan 24, 2022, 10:28 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, string argv[]){
@@ -11,9 +12,9 @@ int main(int argc, string argv[]){
     char k[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-        for (int i=0;i<strlen(plaintext);i++)
+        for (i=0;i<strlen(plaintext);i++)
     {
-        printf("Ciphertext:%c",k[i]);
+        printf("%c",k[i]);
     }
 
 
