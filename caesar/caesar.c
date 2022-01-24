13c81a2c84a4272ba1da9e3a7b8d399e8a00f437
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 21:12:50 +0000

    Mon, Jan 24, 2022, 9:12 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -11,4 +11,5 @@ int main(int argc, int argv[1]){
     char letters[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
+    printf("Ciphertext: %c", c)
 }
