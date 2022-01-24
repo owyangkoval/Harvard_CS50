9b6c07b4ecc119f015a0fbffa3dde641825c88d2
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 21:23:00 +0000

    Mon, Jan 24, 2022, 9:23 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,12 +5,16 @@
 
 int main(int argc, string argv[]){
 
-    string plaintext = get_string("Plaintext: ");
+string plaintext = get_string("Plaintext: ");
+
+    if (argc == 2)
+    {
+        printf("Plaintext:%s\n", plaintext);
+    }
 
     int k, p, c;
     char letters[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 
-    printf("Ciphertext: %c", c)
 }
