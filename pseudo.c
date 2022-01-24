240bf0c6db9675a191955aba82de71c0b0204dc8
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-24 20:47:48 +0000

    Mon, Jan 24, 2022, 8:47 PM +00:00

---
+++ pseudo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    printf("%d %d %d\n",(int) c1, (int) c2, (int) c3);
+
+}
