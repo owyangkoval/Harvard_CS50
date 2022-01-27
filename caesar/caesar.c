c85261e651e954b0a365a8910dbeb75be7ee04fa
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-27 23:18:15 +0000

    Thu, Jan 27, 2022, 11:18 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,6 +10,8 @@ int main(int argc, string argv[1]){
     //get word plaintext
     string plaintext=get_string("Plaintext: \n")
 
+    k=argc[2]
+
     //plaintext array
     int i[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
