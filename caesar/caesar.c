b65f9c65fcf71499f1a6aa12553d35abadbbf04a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-27 21:50:39 +0000

    Thu, Jan 27, 2022, 9:50 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[2]){
     int plain[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-        for (i=0;i<=52;i++)
+        for (i=0;i<argc;i++)
             {
             cipher=(plain[i]+argc)%26;
             }
