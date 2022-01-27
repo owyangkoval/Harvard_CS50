de0251a3ffedd6f0d7b42ac154f0de907cd0e49f
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-27 23:19:21 +0000

    Thu, Jan 27, 2022, 11:19 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -13,13 +13,13 @@ int main(int argc, string argv[1]){
     k=argc[2]
 
     //plaintext array
-    int i[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
+    int plain[52] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
     'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
         for (i=0;i<52;i++){
             if(k<10)
             {
-
+                cipher[i]==(plain[i]+argc)
             }
             printf("%s\n",cipher);
         }
