67440cdfa72a68e1cc43cde05444bdee3f398316
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 18:55:47 +0000

    Fri, Jan 28, 2022, 6:55 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,24 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 
 int main(void);
 
 int i;
 
-char L[1000];
-char s[1000];
 
+int count_letters(string text);
 get_string("Text: ")
 
-    for(i=0;s[i];i++)
-    {
-    	if(s[i]==32)
-    	 words++;
- 	}
- 	if(i>0)
- 	 words++;
-
-    printf("no of words in string = %d\n",words);
-
-    return 0;
-}
