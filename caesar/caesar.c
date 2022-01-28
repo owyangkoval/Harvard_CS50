e9558f1862c56f6676f0e6cb971a9451f442edf5
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 22:56:20 +0000

    Fri, Jan 28, 2022, 10:56 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,27 +5,18 @@
 
 int main(int argc, string argv[]){
 
-int i, k=0;
-scanf("%i",argv[2])
+int i, c;
 
-    char p[52]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char c[52]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    string s = get_string("Plaintext: ");
+    scanf("%s\n", s);
 
-    string plain = get_string("Plaintext: ");
-    scanf("%s\n", plain);
 
-    //Upper Case Alphabet
-    for(i=65;i<=90;i++){
-        c[i]=(p[i]+k);
+    //Upper and Lower Case Alphabet
+    for(i=0;i<strlen(s);i++){
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')){
+        c[i]=(s[i]+k);
         printf("%c\n",c[i]);
-    }
-
-    //Lower Case Alphabet
-    for(i=97;i<=122;i++){
-        c[i]=(p[i]+k);
-        printf("%c\n",c[i]);
+        }
     }
 
 
