53937845496aa48f2661dfc6fa4a01b39270db5a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 19:16:14 +0000

    Fri, Jan 28, 2022, 7:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,15 +7,18 @@ int count_sentences(string text);
 
 int main(void){
 
-int i;
 char l[52] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
         'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 string name = get_string("Text: ");
 
 int count_letters(string text);
-    for(i=0;i<l[52];i++)
+{
+    int i = 0;
+    while (l[i] != '\0')
     {
-        printf("%d",i);
+        i++;
     }
+    return i;
+}
 }
