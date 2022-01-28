42e60e609cd56f19130aca7513230c31e4a8afea
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 21:12:26 +0000

    Fri, Jan 28, 2022, 9:12 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,9 +11,10 @@ int count_sentences(string text);
     //prompt user for text
     string s = get_string("Text: ");
     printf("Output: \n");
-    for (int i = 0, n = strlen(s); i < n; i++)
-            {
-        printf("%i", n);
+    for (int i = 0; i < strlen(s); i++){
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            printf("%i", i);
         }
-            printf("\n");
+    }
 }
