e26c8b5659a0a812793923676de72f22e1224fd8
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 18:53:16 +0000

    Fri, Jan 28, 2022, 6:53 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 #include<cs50.h>
-#include<string.h>
-#include<math.h>
 
 int main(void);
 
@@ -10,18 +8,15 @@ int i;
 char L[1000];
 char s[1000];
 
-   string text=get_string("Text: ")
+get_string("Text: ")
 
     for(i=0;s[i];i++)
     {
     	if(s[i]==32)
     	 words++;
-
  	}
  	if(i>0)
  	 words++;
-
-
 
     printf("no of words in string = %d\n",words);
 
