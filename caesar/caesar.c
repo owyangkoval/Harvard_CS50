c47b109cda7f796953c8afff1aaf8cc626b317bf
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 23:01:00 +0000

    Fri, Jan 28, 2022, 11:01 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,17 +5,17 @@
 
 int main(int argc, string argv[]){
 
-int i, c;
-
     string s = get_string("Plaintext: ");
-    scanf("%s\n", s);
+
+int i = atoi(s);
+int c;
 
 
     //Upper and Lower Case Alphabet
     for(i=0;i<strlen(s);i++){
         if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')){
         c[i]=(s[i]+k);
-        
+
         printf("%s\n",c[i]);
         }
     }
