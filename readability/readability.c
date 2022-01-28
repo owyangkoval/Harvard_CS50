40d88f9bff70bb76cd0ec792ef23fb35f27e5b40
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 19:01:37 +0000

    Fri, Jan 28, 2022, 7:01 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,19 +2,22 @@
 #include<cs50.h>
 #include<string.h>
 
-int string_length(string s);
 int count_letters(string text);
+int count_sentences(string text);
 
 int main(void){
 
-int i;
+int i=0;
 {
-    string name = get_string("Name: ");
+    string name = get_string("Text: ");
 
-    int i = 0;
-    while (name[i] != '\0')
+int string_length(string s)
+    {
+  
     {
         i++;
     }
-    printf("%i\n", i);
+    return i;
+    }
+}
 }
