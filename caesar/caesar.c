93b79e3c9658f90e0c262a61c720d1edcac6dab9
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 18:18:26 +0000

    Fri, Jan 28, 2022, 6:18 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,6 +6,7 @@
 int main(int argc, string argv[]){
 
 int i, k=0;
+scanf("%i",argv[2])
 
     char p[52]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
