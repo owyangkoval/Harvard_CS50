51f285aa879bf9c6bf9b1bb6bbbecb2da8cba8eb
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-28 21:04:19 +0000

    Fri, Jan 28, 2022, 9:04 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<cs50.h>
 #include<string.h>
+#include<math.h>
 
 int main(void){
 int l;
 int count_letters(string text);
 int count_sentences(string text);
 
-{
+    //prompt user for text
     string s = get_string("Text: ");
     printf("Output: \n");
     for (l = 0, n = strlen(s); l < n; l++)
