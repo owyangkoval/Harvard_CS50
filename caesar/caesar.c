b85dccb6b59b364ff54fbc3e1b8722ef346af5a2
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-31 17:23:15 +0000

    Mon, Jan 31, 2022, 5:23 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -11,9 +11,11 @@ int i = atoi(s);
 
     //Getting int values for a-z through ASCII
     for(i=0;i<=strlen(s);i++){
-        while(s[i]<=96||s[i]>=91)
+     while(s[i]<=96 && s[i]>=91)
         {
                   i++;
+            printf("%c ", i);
+            i++;
         }
         if(s[i] ==' ')
         {
