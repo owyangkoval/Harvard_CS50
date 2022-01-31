69ab5dce05c041e9650a7fb4098903d4378e4793
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-31 19:05:49 +0000

    Mon, Jan 31, 2022, 7:05 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -13,6 +13,12 @@ int j;
     //Getting int values for a-z through ASCII
     for(i=0;i<=strlen(s);i++)
         {
+        if(s[i]=='\0')
+            {
+                s[i]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' }
+                printf("%c",s[i]);
+            }
         //spaces stay the same
         if(s[i]==' ')
             {
