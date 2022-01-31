1af7b2d28f971c81534a6c5ab1e0e91ec6ca6232
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-31 20:54:29 +0000

    Mon, Jan 31, 2022, 8:54 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -18,23 +18,17 @@ int j;
                 s[i]=s[i];
                 printf("%c",s[i]);
             }
-        else if(s[i] >= 'a' && s[i] <= 'z')
+            }
+        else if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
             {
                 //Formula for cipher using key
-                s[i]=(s[i]+2);
+                s[i]=(s[i]+1);
                 printf("%c",s[i]);
             }
-        else if(s[i] >= 'A' && s[i] <= 'Z')
+        else if((s[i] >='{' && s[i] <= '@') || (s[i] >= '[') && s[i] <= '`'))
             {
-                //Formula for cipher using key
-                s[i]=(s[i]+2);
-                  printf("%c",s[i]);
-            }
-        else if((s[i] <= '@' && s[i] >='{') || (s[i] <= '`' && s[i] >= '['))
-            {
-                s[i]=s[i];
+                i++;
                 printf("%c",s[i]);
             }
-        }
     printf("\n");
 }
