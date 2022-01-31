3605e743a75970fb827ca7a9662644e685b57b70
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-31 17:10:46 +0000

    Mon, Jan 31, 2022, 5:10 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,19 +7,25 @@ int main(int argc, string argv[]){
 
 string s = get_string("Plaintext: ");
 
-int isspace(int argument);
+
 
 
 int i = atoi(s);
 
     //Getting int values for a-z through ASCII
     for(i=0;i<=strlen(s);i++){
-        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || s[i] ==' '){
-        //Formula for cipher using key
-        s[i]=(s[i]+1);
-        printf("%c",s[i]);
-        int isspace(int argument);
+        if(s[i] ==' ')
+        {
+            s[i]=s[i];
+            printf("%c",s[i]);
         }
+        else if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            //Formula for cipher using key
+            s[i]=(s[i]+1);
+            printf("%c",s[i]);
+        }
+
     }
         printf("\n");
 
