def9368aaf44c8e45729cac43112003d07615f9a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-01-31 21:10:02 +0000

    Mon, Jan 31, 2022, 9:10 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -22,7 +22,7 @@ int j;
             {
                 //Formula for cipher using key
                 s[i]=(s[i]+2);
-                    while(s[i] <='/0')
+                s[i] = '\0';
                 printf("%c",s[i]);
             }
     }
