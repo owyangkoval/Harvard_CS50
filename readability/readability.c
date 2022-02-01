0689d4284bce3246e7dc0909569f6067903dfb54
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-01 21:48:41 +0000

    Tue, Feb 1, 2022, 9:48 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,5 +8,11 @@ int main(void){
 string text = get_string("Text: ");
 printf("%s\n",text);
 
+int i;
+int count_letters(string text)
 
-}
+    //counting letters
+    for(i=0;i<strlen(text);i++)
+        {
+            printf(")
+        }
