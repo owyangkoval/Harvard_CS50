d0e70ff56d4493febafaa2db21d22576cc5e29bb
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-02 01:10:20 +0000

    Wed, Feb 2, 2022, 1:10 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,8 +11,9 @@ int main(void){
 string text = get_string("Text: ");
 printf("%s\n",text);
 
-int i; int letters=0; int letteravg=0; int words=1; int sentences=0; sentenceavg=0;
-float grade;
+int i; int letters=0; int words=1; int sentences=0;
+float letteravg=0; float sentenceavg=0;
+int grade;
 
 int count_letters(string text);
 int count_words(string text);
@@ -21,7 +22,7 @@ int count_words(string text);
     for(i = 0; i < strlen(text); i++){
         if(isalpha(text[i]))
             {
-            letters = letters+1;
+                letters = letters+1;
             }
         }
             printf("%i letters",letters);
@@ -32,7 +33,7 @@ int count_words(string text);
     for(i = 0; i < strlen(text); i++){
         if((text[i])==' ')
             {
-                words = words+1;
+                    words = words+1;
             }
         }
             printf("%i words", words);
@@ -43,7 +44,7 @@ int count_words(string text);
     for(i = 0; i < strlen(text); i++){
         if((text[i])=='.' || (text[i])=='?' || (text[i])=='!')
             {
-                sentences = sentences+1;
+                    sentences = sentences+1;
             }
         }
             printf("%i sentences", sentences);
@@ -52,15 +53,16 @@ int count_words(string text);
     //letter average
     for(i = 0; i < strlen(text); i++){
             {
-            letteravg==(letters/words)*100;
+                letteravg=(letters/words)*100;
             }
         }
     //sentence average
     for(i = 0; i < strlen(text); i++){
             {
-            sentenceavg==(sentences/words)*100;
+                sentenceavg=(sentences/words)*100;
             }
         }
+        printf("%f\n", sentenceavg);
 
 
 }
