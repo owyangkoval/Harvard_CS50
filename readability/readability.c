b77e715da6b9cefcac9f3bbd40d7293f70d9db21
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-02 19:15:32 +0000

    Wed, Feb 2, 2022, 7:15 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,7 +12,7 @@ string text = get_string("Text: ");
 printf("%s\n",text);
 
 int i; int letters=0; int words=1; int sentences=0;
-float letteravg; float sentenceavg;
+float letteravg=0; float sentenceavg=0;
 int grade;
 
 int count_letters(string text);
@@ -25,35 +25,47 @@ int count_words(string text);
                 letters = letters+1;
             }
         }
-            printf("%i letters\n",letters);
+            printf("%i letters",letters);
            printf("\n");
 
 
-    //count words and letter average
+    //count words
     for(i = 0; i < strlen(text); i++){
         if((text[i])==' ')
             {
                     words = words+1;
-                    letteravg=letters/words;
-                    letteravg=letteravg*100;
             }
         }
-            printf("%i words\n", words);
-            printf("%f\n", letteravg);
+            printf("%i words", words);
            printf("\n");
 
 
-    //count sentences and sentence average
+    //count sentences
     for(i = 0; i < strlen(text); i++){
         if((text[i])=='.' || (text[i])=='?' || (text[i])=='!')
             {
                     sentences = sentences+1;
-                    sentenceavg=sentences/words;
-                    sentenceavg=sentenceavg*100;
+
             }
         }
-            printf("%i sentences\n", sentences);
-            printf("%f\n", sentenceavg);
+            printf("%i sentences", sentences);
            printf("\n");
+
+    //letter average
+    for(i = 0; i < strlen(text); i++){
+            {
+                letteravg=(letters/words)*100;
+            }
+        }
+        printf("%f\n", letteravg);
+
+    //sentence average
+    for(i = 0; i < strlen(text); i++){
+            {
+                sentenceavg=(sentences/words)*100;
+            }
+        }
+        printf("%f\n", sentenceavg);
+
 
 }
