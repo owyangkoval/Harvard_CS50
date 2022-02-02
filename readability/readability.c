1d29d14ab0b226c839cc8f673fc4e85ccd1f8cdd
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-02 19:23:47 +0000

    Wed, Feb 2, 2022, 7:23 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,7 +11,7 @@ int main(void){
 string text = get_string("Text: ");
 printf("%s\n",text);
 
-int i; int letters=0; int words=1; int sentences=0;
+int i; int letters=0; int words=1; int sentences=0; int index;
 float letteravg=0; float sentenceavg=0;
 int grade;
 
@@ -61,4 +61,11 @@ int count_words(string text);
         printf("%f\n", letteravg);
         printf("%f\n", sentenceavg);
 
+    //coleman-liau
+    for(i = 0; i < strlen(text); i++){
+        {
+            index=(0.0588*letteravg)-(0.296*sentenceavg)-15.8;
+        }
+    }
+        printf("Grade %i\n", index);
 }
