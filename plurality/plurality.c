c33e8ccfab62dbf8b28326931e5b23cf430f7046
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-08 19:10:18 +0000

    Tue, Feb 8, 2022, 7:10 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -53,7 +53,7 @@ int main(int argc, string argv[])
         string name = get_string("Vote: ");
 
         // Check for invalid vote
-        if (strcmp(argv[i] == 0)
+        if (strcmp(candidates[i].name, "%s",argv[i] == 0)
         {
             return 0;
         }
