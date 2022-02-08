152ea3e521bf8a5dbdd30112f10f8cdd5b78c768
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-08 19:00:00 +0000

    Tue, Feb 8, 2022, 7:00 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -44,7 +44,7 @@ int main(int argc, string argv[])
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
     }
-    
+
     int voter_count = get_int("Number of voters: ");
 
     // Loop over all voters
@@ -57,6 +57,7 @@ int main(int argc, string argv[])
         {
             printf("Invalid vote.\n");
         }
+        else if (vote)
     }
 
     // Display winner of election
