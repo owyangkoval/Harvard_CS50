0d3b5cd575373abec82ebdbeaee1fbe749f92300
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-09 18:46:18 +0000

    Wed, Feb 9, 2022, 6:46 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,8 +67,9 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    int i=0;
-       if (strcmp(candidates[i].name, name) == 0)
+        int i=0;
+            for (int i = 0; i < voter_count; i++)
+        if (strcmp(candidates[i].name, name) == 0)
             {
                 candidates[i].votes++;
                 return true;
