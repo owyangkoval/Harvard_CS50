ef1490ccdb07c67a1368abc9752fe95f8e2ac74b
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-10 19:00:56 +0000

    Thu, Feb 10, 2022, 7:00 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,11 +82,8 @@ bool vote(string name)
 void print_winner(void)
     {
     // TODO
-    int i=0;
-    int vote_count=0;
-    for(i=0;i<vote_count;i++){
-        if (candidates[i].votes<candidates[i+1].votes)
-            candidates[i].votes=candidates[i+1].votes;
+    if (candidates[i].votes<candidates[i+1].votes)
+        candidates[i].votes=candidates[i+1].votes;
     }
     printf("%s\n", candidates[i].name);
 
