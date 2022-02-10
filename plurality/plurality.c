d0118607f23b584b835db7407424fa5b7ab654a2
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-10 21:24:40 +0000

    Thu, Feb 10, 2022, 9:24 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -91,6 +91,7 @@ void print_winner(void)
         else if (candidates[i].votes>candidates[i+1].votes)
             candidates[i].votes=candidates[i].votes;
         }
+        count++;
     printf("%d\n", candidates[i].votes);
     return;
 }
