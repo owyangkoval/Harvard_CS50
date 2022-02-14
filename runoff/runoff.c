5e94dde07fd51afe316b19e099607c50ae59ed9d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-14 21:59:47 +0000

    Mon, Feb 14, 2022, 9:59 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,7 +143,7 @@ void tabulate(void)
 {
     // TODO
         for (int i = 0; i <voter_count; i++){
-            if (preferences[i][0] > )
+            if (preferences[i][0] > preferences[i][1] )
         }
     return;
 }
