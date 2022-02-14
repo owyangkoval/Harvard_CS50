e35240e941239797b0fa10aaea4ccaa8359abfb1
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-14 19:14:50 +0000

    Mon, Feb 14, 2022, 7:14 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     // TODO
     for (int i = 0; i < candidate_count; i++){
         if(strcmp(candidates[i][j].name, name) == 0)
-        candidates[i][j].votes++;
+            candidates[i][j].votes++;
         for (int j=0; j<voter_count; j++){
             if()
 
