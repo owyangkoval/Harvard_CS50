43ee432f57b876ea0cdb0f258f9dc8f04fec35ff
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-16 04:34:39 +0000

    Wed, Feb 16, 2022, 4:34 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -186,7 +186,7 @@ bool is_tie(int min)
     // TODO
 
     for (int i = 0; i < candidate_count - 1; i++) {
-        if (candidates[i].votes != candidates[i+1].votes) {
+        if (candidates[i].votes != min) {
             return false;
         }
     }
