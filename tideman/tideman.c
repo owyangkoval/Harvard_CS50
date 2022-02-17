b857934a4609bf717710c03c36b399e2e59b082d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-17 23:03:50 +0000

    Thu, Feb 17, 2022, 11:03 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
     for (int i = 0; i < candidate_count; i++) {
         for (int j = 0; j < _count; j++) {
             if (i > j) {
-                ranks[rank]
+                ranks[preferences[i][j]]
             }
         }
     }
