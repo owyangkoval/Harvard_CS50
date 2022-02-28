85ef93d7c5de2fff434b3358bb227ddc709f7c5a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-02-28 19:38:54 +0000

    Mon, Feb 28, 2022, 7:38 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,7 @@ void add_pairs(void)
 {
     // TODO
     for (int i = 0; i < candidate_count; i++) {
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < candidate_count; j++) {
         if (strcmp(preferences[i][j], preferences[i][j])==0);
             preferences[i][j]
 
