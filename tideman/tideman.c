3d7cb6c25064f4a80599e0a264f33a6b4bb88fc8
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-01 00:28:21 +0000

    Tue, Mar 1, 2022, 12:28 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,7 +134,7 @@ void add_pairs(void)
     for (int i = 0; i < candidate_count; i++) {
         for (int j = 0; j < candidate_count; j++) {
             if (ranks[rank])
-
+            pair_count++;
         }
     }
     return;
