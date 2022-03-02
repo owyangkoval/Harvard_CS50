6c1031868eb1e87567231f1c1d943dcc7be3c855
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-02 00:17:09 +0000

    Wed, Mar 2, 2022, 12:17 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -150,7 +150,7 @@ void sort_pairs(void)
     int unsorted_pair;
     int sorted_pair;
 
-    for (int i = 0; i < pair_count; i++) {
+    for (int i = 0; i < pair_count-1; i++) {
         for (int j = i + 1; j < pair_count; j++) {
                      if(unsorted >
                      strongestpair =
