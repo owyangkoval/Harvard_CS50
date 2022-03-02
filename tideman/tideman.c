45dc8d692d6603ec2fd79e9a1723e87afdfa1d4b
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-02 00:40:20 +0000

    Wed, Mar 2, 2022, 12:40 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -131,15 +131,20 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++) {
         for (int j = i + 1; j < candidate_count; j++) {
-            if (i > j) {
-            i = preferences[i][j];
-            j = preferences[j][i];
-            pairs[i].winner = i;
-            pairs[j].loser = j;
-            pair_count++;
-            }
+                if (preferences[i][j] > preferences[j][i])
+                {
+                    pairs[pair_count].winner = i;
+                    pairs[pair_count].loser = j;
+                    pair_count++;
+                }
+
+                else if (preferences[i][j] < preferences[j][i])
+                {
+                    pairs[pair_count].winner = j;
+                    pairs[pair_count].loser = i;
+                    pair_count++;
+                }
         }
-    }
     return;
 }
 
