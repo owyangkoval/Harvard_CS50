a6b3a90fbff1553ce6a968fde2f114d7ada64b94
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-02 19:10:00 +0000

    Wed, Mar 2, 2022, 7:10 PM +00:00

--- practice.c
+++ practice.c
@@ -6,18 +6,18 @@ int main (void)
 {
 int array[4] = {5, 3, 1, 9};
 
-   for (int i=0; i<3; i++) {
+   for (int i=0; i<=3; i++) {
         int min = i;
-            for (int j=i+1; j<4; j++)  {
+            for (int j=i+1; j<=4; j++)  {
                 if (array[j] < array[min]) {
                     array[j] = array[min];
                     min = j;
-                            printf("%d", array[min]);
                     }
                     int temp = array[i];
                     array[i] = array[min];
                     array[min] = temp;
                 }
+                    printf("%d", array[min]);
             }
         printf("\n");
 }
