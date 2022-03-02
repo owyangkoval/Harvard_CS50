b3b42a63ff5eeab636c4f472e34b880a1cb3070a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-02 01:44:11 +0000

    Wed, Mar 2, 2022, 1:44 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -151,9 +151,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
     int max = i;
-
     for (int i = 0; i < pair_count; i++) {
         for (int j = i + 1; j < pair_count; j++) {
            if (preferences[pairs[j].winner][pairs[j].loser] > preferences[pairs[i].winner][pairs[i].loser])
