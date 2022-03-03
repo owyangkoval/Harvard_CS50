5bc734e12c8b26222174c4ccfaf7fe89e89f2792
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-03 20:09:47 +0000

    Thu, Mar 3, 2022, 8:09 PM +00:00

--- insertionsort.c
+++ insertionsort.c
@@ -7,4 +7,11 @@ int main (void)
 int array[4] = {5, 3, 1, 9};
 int i,j,k,temp;
 
+   for (i=1; i<3; i++) {
+        int min = i;
+            for (j=i+1; j<4; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                    }
+                }
 
