f457acb98c68e1f4b6275f0f494553b74ecdc6cf
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-03 18:07:04 +0000

    Thu, Mar 3, 2022, 6:07 PM +00:00

--- bubblesort.c
+++ bubblesort.c
@@ -8,17 +8,11 @@ int array[4] = {5, 3, 1, 9};
 int i,j,k,temp;
 
     for(i=0;i<3;i++) {
-        int max = i;
             for(j=i+1;j<4;j++) {
-                if(array[max]>array[j]) {
-                    max = j;
+                if(array[i]>array[j]) {
+                    i = j;
                         }
-                }
-                    if (max != j) {
-                    temp = array[max];
-                    array[max] = array[j];
-                    array[j] = temp;
-                        }
+                    }
                 }
             for(k=0;k<4;k++){
             printf("%d", array[k]);
