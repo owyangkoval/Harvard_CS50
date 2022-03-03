1a2ad81f0b436e1762ac7b6f47bac576f1760b66
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-03 18:47:46 +0000

    Thu, Mar 3, 2022, 6:47 PM +00:00

--- bubblesort.c
+++ bubblesort.c
@@ -5,19 +5,18 @@
 int main (void)
 {
 int array[4] = {5, 3, 1, 9};
-int i,j,k,temp;
+int i,j,temp;
 
     for(i=0;i<3;i++) {
             for(j=0;j<4;j++) {
                 if(array[j]>array[j+1]) {
-                    j = j+1;
-                        }
                     temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
                     }
-            for(k=0;k<4;k++){
-            printf("%d", array[k]);
+                }
+            for(i=0;i<4;i++){
+            printf("%d", array[i]);
             }
          }
         printf("\n");
