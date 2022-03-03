ecbee57f2537d3cbdf06ee60f9fd9d0276591aff
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-03 16:40:14 +0000

    Thu, Mar 3, 2022, 4:40 PM +00:00

---
+++ code practice/practicebubblesort.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+int array[4] = {5, 3, 1, 9};
+int i,j,k,temp;
+
+    for(i=0;i<3;i++) {
+        int min = i;
+            for(j=i+1;i<4;i++) {
+                if(array[j]<array[min]) {
+                    min = j;
+                }
+                if (min!=i) {
+                    
+                }
+            }
+    }
+
+        printf("\n");
+    }
