7e5d851efb4c90663b319b5f2cc11adf5916868a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-07 23:51:05 +0000

    Mon, Mar 7, 2022, 11:51 PM +00:00

---
+++ linearsearch.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+int array[4] = {5, 3, 1, 9};
+int i;
+
+for (i=0;i<4;i++) {
+    if(array[i]==9) {
+        printf("Index found at %d\n",i);
+        }
+    }
+}
