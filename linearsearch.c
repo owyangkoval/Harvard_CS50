1296720ba9b34629efe1c26ac39ac877f921a45b
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-08 00:05:04 +0000

    Tue, Mar 8, 2022, 12:05 AM +00:00

--- linearsearch.c
+++ linearsearch.c
@@ -6,13 +6,14 @@ int main (void)
 {
 int array[4] = {5, 3, 1, 9};
 int i;
+int found=0;
 
 for (i=0;i<4;i++) {
     if(array[i]==8) {
         printf("Index found at %d\n",i);
         }
-        else if (i!=0) {
+    }
+        else (found==0) {
         printf("Element not found\n");
         }
-    }
 }
