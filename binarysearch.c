f1b3ec15160da7686948515b37889e42cd6206e9
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-09 18:52:04 +0000

    Wed, Mar 9, 2022, 6:52 PM +00:00

--- binarysearch.c
+++ binarysearch.c
@@ -6,24 +6,24 @@ int main (void)
 {
 int array[6] = {1, 2, 3, 7, 9, 10};
 int l=0;
-int r=6;
+int r=5;
 int i=0;
 int mid = (l+r)/2;
 
 while(l<=r){
-    if(array[mid]==) {
+    if(array[mid]==2) {
         return mid;
-        printf("Index found at %d\n", mid);
+        printf("Index found at %d\n", array[mid]);
         break;
             }
-    if (array[mid]>9) {
+    if (array[mid]>2) {
         r = mid-1;
-        printf("Index found at %d\n", mid-1);
+        printf("Index found at %d\n", array[r]);
         break;
             }
     else {
         l = mid+1;
-        printf("Index found at %d\n", mid+1);
+        printf("Index found at %d\n", array[l]);
         break;
             }
         }
