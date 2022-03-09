7f70e30766964637e210734a595d5aeeb6b79502
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-09 20:00:03 +0000

    Wed, Mar 9, 2022, 8:00 PM +00:00

--- binarysearch.c
+++ binarysearch.c
@@ -7,9 +7,9 @@ int main (void)
 int array[6] = {1, 2, 3, 7, 9, 10};
 int l=0;
 int r=5;
-int mid = (l+r)/2;
 
 while(l<=r){
+    int mid = (l+r)/2;
     if(array[mid]==9) {
         printf("Index found at %d\n", mid);
         break;
