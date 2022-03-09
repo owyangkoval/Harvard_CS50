db9df45db2882e81adcaa1aef7fc77fc0a00fa5d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-09 23:59:56 +0000

    Wed, Mar 9, 2022, 11:59 PM +00:00

--- quicksort.c
+++ quicksort.c
@@ -2,34 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function prototypes
-quicksort
+void quicksort(int array[], int start, int end) {
+int array[start] = 0;
+int array[end] = 5;
+int pivot; int temp;
 
-int void (main)
+    while(array[start]<=pivot) {
+        start++;
+        }
+    while(array[end]>pivot) {
+        end--;
+        }
+    if (start<end) {
+        int temp=array[end];
+        array[end]=array[start];
+        array[start]=temp;
+        }
+    if (start>end) {
+        temp=array[start];
+        array[start]=array[end];
+        array[end]=temp;
+        }
+}
+
+int main(void) {
     int array[6] = {2, 8, 9, 1, 3, 4};
-    int pivot = array[0];
     for(int k=0;k<6;k++){
         printf("%d", array[k]);
             }
         printf("\n");
 }
-
-void quicksort(int array[], array[start], array[end]) {
-int temp;
-
-while(array[start]<=pivot) {
-        start++;
-        }
-while(array[end]>pivot) {
-        end--;
-        }
-if (start<end) {
-        temp=array[end];
-        array[end]=array[start];
-        array[start]=temp;
-        }
-if (start>end) {
-        temp=array[start];
-        array[start]=array[end];
-        array[end]=temp;
-        }
