b9d5f11d18fc2eba94226890c79124208d139210
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-09 22:22:41 +0000

    Wed, Mar 9, 2022, 10:22 PM +00:00

--- quicksort.c
+++ quicksort.c
@@ -4,4 +4,28 @@
 
 int main (void)
 {
-int array[6] = {1, 2, 3, 7, 9, 10};
+int array[6] = {2, 8, 10, 1, 3, 4};
+int start = 0;
+int end = 5;
+int pivot = 0;
+
+while (start>end) {
+    while(array[start]<=pivot) {
+        start++;
+        }
+    while(array[end]>pivot) {
+        end--;
+        }
+    if (start<end) {
+        temp=array[end];
+        array[end]=array[start];
+        array[start]=temp;
+        }
+    }
+    if (start>end) {
+        temp=array[start];
+        array[start]=array[end];
+        array[end]=temp;
+        return end;
+    }
+}
