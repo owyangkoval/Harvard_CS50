fa776d09b2dbebd2582b4b56b7732c2451e6d112
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-09 23:45:31 +0000

    Wed, Mar 9, 2022, 11:45 PM +00:00

--- quicksort.c
+++ quicksort.c
@@ -7,28 +7,26 @@ int main (void)
 int array[6] = {2, 8, 9, 1, 3, 4};
 int start = 0;
 int end = 5;
-int pivot = 0;
+int pivot = array[0];
 int temp;
 
-while (start>end) {
-    while(array[start]<=pivot) {
+while(array[start]<=pivot) {
         start++;
         }
-    while(array[end]>pivot) {
+while(array[end]>pivot) {
         end--;
         }
-    if (start<end) {
+if (start<end) {
         temp=array[end];
         array[end]=array[start];
         array[start]=temp;
         }
-    }
-    if (start>end) {
+if (start>end) {
         temp=array[start];
         array[start]=array[end];
         array[end]=temp;
         return end;
-    }
+        }
     for(int k=0;k<6;k++){
         printf("%d", array[k]);
             }
