c809486efaad17a185abd375df9c4bf3987484f4
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-14 18:36:12 +0000

    Mon, Mar 14, 2022, 6:36 PM +00:00

--- quicksort.c
+++ quicksort.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void quicksort(int array[], int lowerbound, int upperbound) {
+void partition(int array[], int lowerbound, int upperbound) {
 int pivot=0, start=lowerbound, end=upperbound, temp;
 
 while(start<end) {
@@ -23,6 +23,16 @@ while(start<end) {
     array[end]=temp;
     return end;
 }
+
+void quiksort (int array[], int lowerbound, int upperbound) {
+    if (lowerbound<upperbound) {
+        loc = partition(array, upperbound, lowerbound);
+        
+    }
+
+}
+
+
 
 int main(void) {
     int array[6] = {2, 8, 9, 1, 3, 4};
