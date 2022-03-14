700ffceac7ac765e0dac4896d4a46b99d9996463
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-14 23:21:42 +0000

    Mon, Mar 14, 2022, 11:21 PM +00:00

--- recursion.c
+++ recursion.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
-int factorial(int n) {
-   //base case
-   if(n == 0) {
-      return 1;
-   } else {
-      return n * factorial(n-1);
-   }
-}
-
 int fibbonacci(int n) {
-   if(n == 0){
-      return 0;
-   } else if(n == 1) {
+if(n == 1) {
       return 1;
    } else {
       return (fibbonacci(n-1) + fibbonacci(n-2));
@@ -20,13 +9,8 @@ int fibbonacci(int n) {
 }
 
 int main() {
-   int n = 55;
    int i;
 
-   printf("Factorial of %d: %d\n" , n , factorial(n));
-   printf("Fibbonacci of %d: " , n);
-
-   for(i = 0;i<n;i++) {
-      printf("%d ",fibbonacci(i));
-   }
+   i = fibbonacci(55);
+      printf("%d ",i);
 }
