65f156adb7a14bce892ccb3920403992308f7bf1
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-14 21:22:08 +0000

    Mon, Mar 14, 2022, 9:22 PM +00:00

---
+++ recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int i) {
+int j=0;
+   if (i<1)
+    return 1;
+   j = i+sum(i-1);
+   return j;
+}
+
+int main(void) {
+    int i=0;
+    int k=sum(1);
+    printf("The sum of numbers from 1 - 5: %d\n", k);
+}
