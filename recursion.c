f1ebc41ea3e4366b68b2e0abc309d7dae26bb3a6
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-14 22:08:06 +0000

    Mon, Mar 14, 2022, 10:08 PM +00:00

--- recursion.c
+++ recursion.c
@@ -4,12 +4,12 @@ int fib(int i) {
 int j=0;
    if (i==1)
     return 1;
-   j = i+fib(i-1);
+   j = fib(i-1)+fib(i-2);
    return j;
+   printf("%d", j);
 }
 
 int main(void) {
     int i=0;
-    int k=fib(1);
-    printf("The sum of numbers from 1 - 5: %d\n", k);
+    int k=fib(55);
 }
