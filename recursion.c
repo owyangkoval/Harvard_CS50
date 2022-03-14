1507218706947c0ced9535e1336f1ee8e65e1769
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-14 22:10:16 +0000

    Mon, Mar 14, 2022, 10:10 PM +00:00

--- recursion.c
+++ recursion.c
@@ -2,14 +2,15 @@
 
 int fib(int i) {
 int j=0;
-   if (i==1)
+   if (i==0)
     return 1;
    j = fib(i-1)+fib(i-2);
    return j;
-   printf("%d", j);
 }
 
 int main(void) {
     int i=0;
     int k=fib(55);
+    printf("%d", k);
+    printf("\n");
 }
