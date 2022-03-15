1cde2ddedd84c999988e4e431997faa0e4bf2cab
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-15 20:58:10 +0000

    Tue, Mar 15, 2022, 8:58 PM +00:00

--- recursion.c
+++ recursion.c
@@ -6,13 +6,13 @@ if(n == 1) {
    }
 else {
    return (fib(n-1)+fib(n-2));
+   printf("%d ",series[n]);
    }
 }
 
 int main() {
    int k;
-   int series[10]={1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
+   int series[10]={1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
    k = fib(series, 55);
-      printf("%d ",k);
 }
