7f6c3a39ad0447326f30e1c1ff5aa503912ef57e
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 17:28:03 +0000

    Wed, Mar 16, 2022, 5:28 PM +00:00

--- recursion.c
+++ recursion.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int even(int i) {
+int evenset(int array[], int i) {
 int j = 0;
-int k = 0;
 
    if(i<=2) {
    return 2;
    }
    else if (i>2) {
-   j = even(i-2);
+   
    return j;
    }
 }
+
 
 int main() {
 int array[6];
