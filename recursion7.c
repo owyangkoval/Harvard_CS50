3ff1ef54dbd1690d71797fdf07b6d1eb6a556b23
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 22:18:40 +0000

    Wed, Mar 16, 2022, 10:18 PM +00:00

--- recursion7.c
+++ recursion7.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
-int secondnum(int j) {
-    if(j<=0)
-    return 0;
-    else {
-    return j;
-    }
-}
-
-int gcdnum(int i) {
-int k=0; int j=0;
-   if (i<=0)
-   return 0;
-   else {
-   k = gcdnum(i/(i/j);
+int gcdnum(int i, int j) {
+int k=0;
+   while (i!=j) {
+   k = gcdnum(i/(j/i));
    return k;
    }
 }
 
 int main(void) {
-int i, j, k, l;
+int i, j, k;
 
    printf("Input 1st number: ");
    scanf("%d",&i);
    printf("Input 2nd number: ");
    scanf("%d",&j);
 
-    k = gcdnum(i);
-    l = secondnum(j);
+    k = gcdnum(i, j)
    printf("The GCD of %d and %d is: %d\n", i,j,k);
+   return 0;
 }
