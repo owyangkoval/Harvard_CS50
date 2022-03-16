26531895aa82db692c1ddb497a7a3c6920c73272
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 20:22:23 +0000

    Wed, Mar 16, 2022, 8:22 PM +00:00

--- recursion.c
+++ recursion.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int digits(int i) {
+int k = 0;
    if (i>=0) {
    printf("Integer: ");
    scanf("%d",&i);
    digits(i/=10);
+   k=k+1;
    }
-   else
-   return i;
+   printf("The number of digits in the number is : %d", k);
+   return k;
 }
 
-int main() {
-   int i;
-   k = digits(i);
-   printf("The number of digits in the number is : ", k);
+
+int main(void) {
+
+   digits(50);
 }
