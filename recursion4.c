f71a6234ca472fa0aacdaf5d6ea6ad531f7b52c1
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 20:01:29 +0000

    Wed, Mar 16, 2022, 8:01 PM +00:00

---
+++ recursion4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int input(int a[],int n) {
+    if(n>=0) {
+    input(a,n-1);
+    printf("\nelement-%d",n);
+    scanf("%d", &a[n]);
+    }
+return 1;
+}
+
+int set(int a[],int n) {
+    if(n>=0) {
+    display(a,n-1);
+    printf("%d ",a[n]);
+    }
+return 1;
+}
+
+int main(void)
+{
+int n;
+printf("Input the number of elements to be stored in the array :");
+scanf("%d", &n);
+printf("\nInput %d elements in the array:",n);
+
+int a[n];
+input(a,n-1);
+printf("\n\nThe elements in the array are: ");
+display(a,n-1);
+}
