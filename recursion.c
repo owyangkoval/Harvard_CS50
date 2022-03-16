2d12da465b199728325efcaaf017bcff3b08a9c2
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 17:37:01 +0000

    Wed, Mar 16, 2022, 5:37 PM +00:00

--- recursion.c
+++ recursion.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
+#define MAX 100
 
-int evenset(int i) {
-int j = 0;
+void ArrayElement(int arr1[], int st, int l);
 
-   if(i<=2)
-      return 2;
-   else
-      return evenset(i-2);
+int main()
+{
+    int arr1[MAX];
+    int n, i;
+	printf("\n\n Recursion : Print the array elements :\n");
+	printf("-------------------------------------------\n");
+
+	printf(" Input the number of elements to be stored in the array :");
+    scanf("%d",&n);
+
+    printf(" Input %d elements in the array :\n",n);
+    for(i=0;i<n;i++)
+        {
+	      printf(" element - %d : ",i);
+	      scanf("%d",&arr1[i]);
+	    }
+
+    printf(" The elements in the array are : ");
+    ArrayElement(arr1, 0, n);//call the function ArrayElement
+    printf("\n\n");
+    return 0;
 }
 
+void ArrayElement(int arr1[], int st, int l)
+{
+    if(st >= l)
+        return;
 
-int main() {
-int array[6];
-int i;
+    //Prints the current array element
+    printf("%d  ", arr1[st]);
 
-   for(i=0; i < 5; ++i) {
-       printf("Enter number %d: ",i);
-      scanf("%d", &array[i]);
-
-   evenset(14);
-
-      printf("The elements in the array are :%d", evenset(i));
-      printf("\n");
-
-      }
+    /* Recursively call ArrayElement to print next element in the array */
+    ArrayElement(arr1, st+1, l);//calling the function  ArrayElement itself
 }
