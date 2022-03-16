306680af8a489b90c28af1210c4c23d312505c35
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-16 22:57:09 +0000

    Wed, Mar 16, 2022, 10:57 PM +00:00

--- recursion8.c
+++ recursion8.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
-int input(int a[], int n)
-
+int input(int a[], int n) {
+    if (n>=0) {
     input(a,n-1);
-    printf("Element - %d: ", n)
-    scanf("%d", &n);
-    return n;
+    printf("Element - %d: ", n);
+    scanf("%d", &a[n]);
+    }
+    return 1;
+}
+
+int highestelement(int a[], int n) {
+int temp;
+    if (n<n+1) {
+        temp = a[n];
+        a[n] = a[n+1];
+        a[n+1] =temp;
+    }
+}
 
 int main(void) {
 
-    input()
+int n;
+printf("Input the number of elements to be stored in the array :");
+scanf("%d", &n);
+printf("\nInput %d elements in the array:",n);
+
+int a[n];
+input(a,n-1);
+printf("\n\nThe highest element of the array is: ");
+highestelement(n);
 
 }
