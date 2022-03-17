96aa564fcf05a9942e20d504402bc06a8135ba27
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-03-17 18:23:37 +0000

    Thu, Mar 17, 2022, 6:23 PM +00:00

--- recursion8.c
+++ recursion8.c
@@ -6,7 +6,7 @@ int n;
 
 int main()
 {
-    int arr1[MAX],hstno,i;
+    int arrayelem[MAX],hstno,i;
       printf(" Input the number of elements to be stored in the array :");
        scanf("%d",&n);
 
@@ -14,7 +14,7 @@ int main()
        for(i=0;i<n;i++)
         {
 	      printf(" element - %d : ",i);
-	      scanf("%d",&arr1[i]);
+	      scanf("%d",&arrayelem[i]);
 	    }
     hstno=MaxElem(arr1);//call the function MaxElem to return the largest element
     printf(" Largest element of the array is: %d\n\n",hstno);
@@ -22,13 +22,13 @@ int main()
 }
 
 
-int MaxElem(int arr1[])
+int MaxElem(int arrayelem[])
 {
     static int i=0,hstno =-9999;
     if(i < n)
     {
-         if(hstno<arr1[i])
-           hstno=arr1[i];
+         if(hstno<arrayelem[i])
+           hstno=arrayelem[i];
       i++;
       MaxElem(arr1);//calling the function MaxElem itself to compare with further element
     }
