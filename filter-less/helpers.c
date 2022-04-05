62b24fbdefe6a0062cfa460e025124539d70dce6
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-05 21:46:01 +0000

    Tue, Apr 5, 2022, 9:46 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-        int average = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue/3.0;
+        int average = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue/3.00;
             image[i][j].rgbtRed = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtBlue = average;
