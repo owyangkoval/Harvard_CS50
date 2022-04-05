cfc198b5d86fbb60d7c72666628bfd0ec10c46f6
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-05 20:49:14 +0000

    Tue, Apr 5, 2022, 8:49 PM +00:00

---
+++ filter-less/helpers.c
@@ -0,0 +1,34 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+    image[i][j].rgbtRed = 0x55;
+    image[i][j].rgbtGreen = 0x55;
+    image[i][j].rgbtBlue = 0x55;
+            }
+        }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
