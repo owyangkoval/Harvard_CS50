0b6ef8650e11059ce0a6c80579a8b8ac58e5844a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-05 22:03:26 +0000

    Tue, Apr 5, 2022, 10:03 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,17 +26,20 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-        float image[i][j].rgbtRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-            if (image[i][j].rgbtRed > 255) {
-            image[i][j].rgbtRed = 255;
+        float image[i][j].rgbtRed = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
+            int red = image[i][j].rgbtRed;
+            if (red > 255) {
+            red = 255;
                 }
-        float image[i][j].rgbtGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
-            if (image[i][j].rgbtGreen > 255) {
-            image[i][j].rgbtGreen = 255;
+        float image[i][j].rgbtGreen = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
+            int green = image[i][j].rgbtGreen;
+            if (green > 255) {
+            green = 255;
                 }
-        image[i][j].rgbtBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
-            if (image[i][j].rgbtBlue > 255) {
-            image[i][j].rgbtBlue = 255;
+        float image[i][j].rgbtBlue = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
+            int blue = image[i][j].rgbtBlue;
+            if (blue > 255) {
+            blue = 255;
             }
         }
     }
