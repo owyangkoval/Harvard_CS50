fef9bf7c30704af9b22d4e4238bbc51ed6882f9f
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-06 18:36:12 +0000

    Wed, Apr 6, 2022, 6:36 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,11 +27,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-        image[i][j].rgbtRed = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
-        image[i][j].rgbtGreen = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
-        image[i][j].rgbtBlue = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
-        if (image[i][j].rgbtRed > ) {
-            image[i][j].rgbtRed = 0xFF;
+        int sepiaRed = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
+        int sepiaGreen = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
+        int sepiaBlue = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
+        image[i][j].rgbtRed = sepiaRed < 255;
         }
         if (image[i][j].rgbtGreen > 0xFF) {
             image[i][j].rgbtGreen = 0xFF;
