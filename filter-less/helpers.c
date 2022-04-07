897457ba14c68e7fdc466f2513b09dfd640ddf9e
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-07 21:52:06 +0000

    Thu, Apr 7, 2022, 9:52 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,10 +70,5 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-       for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width / 2; j++)
-        {
-            
     return;
 }
