a6bdb62f0498e42a3a53d5ebbc97925fd5b7f4a9
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-08 19:02:32 +0000

    Fri, Apr 8, 2022, 7:02 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,11 +70,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-        float red = image[i][j].rgbtRed;
-        float green = image[i][j].rgbtGreen;
-        float blue = image[i][j].rgbtBlue;
-        int k;
-
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
