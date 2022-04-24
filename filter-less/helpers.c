f96e9360c098cd056963496d1446ed6ddad9322a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-24 02:59:00 +0000

    Sun, Apr 24, 2022, 2:59 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -82,10 +82,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            for ( a = i-1; a <= i+1 ; a++) {
-                for (b = j-1; b <=j+1; b++) {
-                    int sum += image[a][b].rgbtRed;
-                    new_image[a][b].rgbtRed = sum/9;
+            if (int a <= (height-1) && a >= 0 && b <= (width-1) && b >= 0) {
+                for ( a = i-1; a <= i+1 ; a++) {
+                    for (b = j-1; b <=j+1; b++) {
+                        int sum += image[a][b].rgbtRed;
+                        new_image[a][b].rgbtRed = sum/9;
+                    }
                 }
             }
         }
