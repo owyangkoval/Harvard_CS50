fda7b920d5de42d40f638fe41e184479c63163c4
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-26 19:20:06 +0000

    Tue, Apr 26, 2022, 7:20 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
 
     //open file card.raw
-    fp = fopen("card.raw", "r");
+    fp = fopen("something.txt", "r");
         if (fp == NULL) {
             printf("error");
             exit(1);
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     //read data
     fread(buffer, 1, 512, fp);
-
+    printf("%s\n", buffer);
     //
 
     fclose(fp);
