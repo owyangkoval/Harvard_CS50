c7617758d4cc9d446056154e5b701392d5bd8716
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-26 23:09:17 +0000

    Tue, Apr 26, 2022, 11:09 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,9 +22,8 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0x6c & buffer[1] == 0x69) {
             int i=0;
             printf("%s\n",buffer);
-            sprintf(fp, "%03i", i);
-            i++;
-            //fwrite(buffer, 1, 8, fp);
+            
+            fwrite(&fp, 1, 8, fp);
             }
     }
 }
