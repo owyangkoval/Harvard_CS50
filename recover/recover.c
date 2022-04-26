aaaf787f3c00326e4f2c9974a578c53ffccf1d6d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-26 21:07:14 +0000

    Tue, Apr 26, 2022, 9:07 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,6 @@ int main(int argc, char *argv[])
 {
     FILE *fp;
     uint8_t buffer[512];
-    char stack = buffer[512];
 
     //open file card.raw
     fp = fopen(argv[1], "r");
@@ -19,7 +18,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, 512, fp) == 512) {
         int i = 0;
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
-        sprintf(char *stack, "%03i.jpg\n", i);
+        sprintf("%03i.jpg\n", i);
         i++;
             }
     else {
