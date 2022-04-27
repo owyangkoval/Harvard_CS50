d1a4503cda82757a13045ed1c32ff7285f47d66f
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 18:20:25 +0000

    Wed, Apr 27, 2022, 6:20 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,14 +19,19 @@ int main(int argc, char *argv[])
     //read data
     while (fread(buffer, 1, 512, fp) == 512) {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
+            // if(first jpg) {
             int i=0;
             sprintf(filename, "%03i.jpg", i)
             i++;
             fwrite(&buffer, 1, 512, fp);
             }
-    //else close file and open new one
+        //else close file and open new one
+            else {
+               //close jpg and start new file;
+                }
+            }
         else {
-            close(fp);
+            
         }
     }
 }
