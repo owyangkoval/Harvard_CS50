e65c1b7f5a12d9f30733bb8a5a7e06e7cef0a918
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 00:47:26 +0000

    Wed, Apr 27, 2022, 12:47 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,8 +20,9 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, 512, fp) == 512) {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
             int i=0;
+            sprintf(filename)
 
-            fwrite(&buffer, 1, 512, fp);
+            //fwrite(&buffer, 1, 512, fp);
 
     }
 }
