0faab022cf02d06c307adeff0b5a3a5465d6207a
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 22:44:32 +0000

    Wed, Apr 27, 2022, 10:44 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, 512, inputfp) == 512)
     {
         printf("read block\n");
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // if first image
             if (i == 0)
