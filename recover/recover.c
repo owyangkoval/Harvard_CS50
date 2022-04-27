0266f822a55d184c0b986fa25d07da6a83c43a6e
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 18:54:11 +0000

    Wed, Apr 27, 2022, 6:54 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
     // read data
     while (fread(buffer, 1, 512, inputfp) == 512)
     {
+        
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0)
         {
             // if first image
