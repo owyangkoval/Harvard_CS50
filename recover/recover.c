555c0963b4681b8be5ab866f6f4ffca3ca4b8859
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 18:42:20 +0000

    Wed, Apr 27, 2022, 6:42 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,29 +5,30 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
+    FILE *inputfp;
+    FILE *outputfp;
     uint8_t buffer[512];
     char filename[8];
 
     //open file card.raw
-    fp = fopen(argv[1], "w+");
-        if (fp == NULL) {
+    inputfp = fopen(argv[1], "w+");
+        if (inputfp == NULL) {
             printf("error");
             exit(1);
         }
 
     //read data
-    while (fread(buffer, 1, 512, fp) == 512) {
+    while (fread(buffer, 1, 512, inputfp) == 512) {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
                 //mark a new image
                 if (int i=0)
                 {
                 sprintf(filename, "%03i.jpg", i)
-                fwrite(&buffer, 1, 512, fp);
+                fwrite(&buffer, 1, 512, outputfp);
                 //else close file and open new one
                     else
                     {
-                    
+
                     i++;
                     }
                 }
