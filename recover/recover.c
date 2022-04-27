fe1435a3393de776185a9b0fbe4591a881f8bb92
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 18:53:28 +0000

    Wed, Apr 27, 2022, 6:53 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,40 +9,44 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
     char filename[8];
 
-    //open file card.raw
-    inputfp = fopen(argv[1], "w+");
-        if (inputfp == NULL) {
-            printf("error");
-            exit(1);
-        }
+    // open file card.raw
+    inputfp = fopen(argv[1], "r");
+    if (inputfp == NULL)
+    {
+        printf("error");
+        exit(1);
+    }
 
-    //read data
-    while (fread(buffer, 1, 512, inputfp) == 512) {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
-                //if first image
-                if (int i=0)
-                {
-                sprintf(filename, "%03i.jpg", i)
+    // read data
+    while (fread(buffer, 1, 512, inputfp) == 512)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0)
+        {
+            // if first image
+            if (i = 0)
+            {
+                sprintf(filename, "%03i.jpg", i);
                 i++;
-                    inputfp = fopen(argv[1], "w+");
-                    if (inputfp == NULL) {
-                        printf("error");
-                        exit(1);
-                        }
+                outputfp = fopen(, "w");
+                if (outputfp == NULL)
+                {
+                    printf("error");
+                    exit(1);
+                }
                 fwrite(&buffer, 1, 512, outputfp);
-                //else close file and open new one
-                    else
-                    {
+                // else close file and open new one
+                else
+                {
                     fclose(outputfp);
-                    }
                 }
             }
-        else
-            {
-            //continue writing to file
-            fwrite(&buffer, 1, 512, fp);
-            }
         }
-    //close file
+        else
+        {
+            // continue writing to file
+            fwrite(&buffer, 1, 512, fp);
+        }
+    }
+    // close file
     fclose(fp);
 }
