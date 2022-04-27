f02f45ca2b9047def70a291de7df8e348900b22c
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-27 18:26:44 +0000

    Wed, Apr 27, 2022, 6:26 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, 512, fp) == 512) {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0) {
                 //mark the beginning of new jpg
-                if(first jpg)
+                //if(first jpg)
                 {
                 int i=0;
                 sprintf(filename, "%03i.jpg", i)
