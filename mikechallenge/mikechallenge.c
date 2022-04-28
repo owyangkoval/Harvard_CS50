eaa2f3e9338415b1404ab400947e69a1c2886045
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:40:52 +0000

    Thu, Apr 28, 2022, 6:40 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 
     FILE *inputfp;
-    int buffer[5];
+    int buffer[5] = {0x54, 0x72, 0x75, 0x6d, 0x70};
     int filename[5];
     int i=0;
 
@@ -19,10 +19,8 @@ int main(int argc, char *argv[])
 
     // read 2016.txt
     while (fread(buffer, 1, 5, inputfp) == 5)
-    {
-        if (buffer[i] == 0x54 && buffer[i + 1] == 0x72 && buffer[i + 2] == 0x75 && buffer[i + 3] == 0x6d && buffer[i + 4] == 70)
         {
-            printf("%c", buffer[i]);
+            printf("%d", *buffer);
             i++;
 //            sprintf(filename, "Biden");
 //           outputfp = fopen("2020.txt", "w");
@@ -33,5 +31,4 @@ int main(int argc, char *argv[])
 //            }
 //            fwrite(&buffer, 1, 512, outputfp);
         }
-    }
 }
