aa67f3723d0ffdbdcba7388f3dd5d335bf9fe7ce
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:23:48 +0000

    Thu, Apr 28, 2022, 6:23 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -5,6 +5,7 @@ int main(int argc, char*argv[]){
 
 FILE *inputfp;
 char *buffer[100];
+char filename[5];
 
        //open 2016.txt
        inputfp = fopen("2016.txt","r");
@@ -15,7 +16,9 @@ char *buffer[100];
 
         //read 2016.txt
         while (fread(buffer, 5, 100, inputfp) == 100){
-            if (buffer[i] == "T" && buffer[i+1] == "r" && buffer [i+2] == "u" && buffer [i+3] == "m" && buffer [i+4] == "p")
-            
+            if (buffer[i] == "T" && buffer[i+1] == "r" && buffer [i+2] == "u" && buffer [i+3] == "m" && buffer [i+4] == "p") {
+            sprintf(filename, "%c%c%c%c%c", );
+            }
+
         }
 }
