5fc08c72ecbeb66c42260391f731715bc2b00e5d
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:25:36 +0000

    Thu, Apr 28, 2022, 6:25 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char*argv[]){
+int main(int argc, char *argv[])
+{
 
-FILE *inputfp;
-char *buffer[100];
-char filename[5];
+    FILE *inputfp;
+    char *buffer[100];
+    char filename[5];
 
-       //open 2016.txt
-       inputfp = fopen("2016.txt","r");
-        if (inputfp == NULL) {
-            printf("error\n");
-            exit(1);
-        }
+    // open 2016.txt
+    inputfp = fopen("2016.txt", "r");
+    if (inputfp == NULL)
+    {
+        printf("error\n");
+        exit(1);
+    }
 
-        //read 2016.txt
-        while (fread(buffer, 5, 100, inputfp) == 100){
-            if (buffer[i] == "T" && buffer[i+1] == "r" && buffer [i+2] == "u" && buffer [i+3] == "m" && buffer [i+4] == "p") {
-            sprintf(filename, "%c%c%c%c%c", );
-                            outputfp = fopen(filename, "w");
-                if (outputfp == NULL)
-                {
-                    printf("error\n");
-                    exit(1);
-                }
-                fwrite(&buffer, 1, 512, outputfp);
+    // read 2016.txt
+    while (fread(buffer, 5, 100, inputfp) == 100)
+    {
+        if (buffer[i] == "T" && buffer[i + 1] == "r" && buffer[i + 2] == "u" && buffer[i + 3] == "m" && buffer[i + 4] == "p")
+        {
+//            sprintf(filename, "%c%c%c%c%c", );
+ //           outputfp = fopen("2020.txt", "w");
+            if (outputfp == NULL)
+            {
+                printf("error\n");
+                exit(1);
             }
-            }
-
+            fwrite(&buffer, 1, 512, outputfp);
         }
+    }
+}
 }
