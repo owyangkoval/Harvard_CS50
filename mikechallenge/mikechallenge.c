6cf589be3ffe9fa85713dc33fc4c637267fd9014
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:28:30 +0000

    Thu, Apr 28, 2022, 6:28 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     // read 2016.txt
     while (fread(buffer, 5, 100, inputfp) == 100)
     {
-        if (buffer[i] == "T" && buffer[i + 1] == "r" && buffer[i + 2] == "u" && buffer[i + 3] == "m" && buffer[i + 4] == "p")
+        if (buffer[i] == 0x54 && buffer[i + 1] == 0x72 && buffer[i + 2] == "u" && buffer[i + 3] == "m" && buffer[i + 4] == "p")
         {
             printf("%s", inputfp);
             i++;
