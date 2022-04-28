8a9b5cedf62cc74da20f4421c2aca47b33d850cf
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:24:54 +0000

    Thu, Apr 28, 2022, 6:24 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -18,6 +18,14 @@ char filename[5];
         while (fread(buffer, 5, 100, inputfp) == 100){
             if (buffer[i] == "T" && buffer[i+1] == "r" && buffer [i+2] == "u" && buffer [i+3] == "m" && buffer [i+4] == "p") {
             sprintf(filename, "%c%c%c%c%c", );
+                            outputfp = fopen(filename, "w");
+                if (outputfp == NULL)
+                {
+                    printf("error\n");
+                    exit(1);
+                }
+                fwrite(&buffer, 1, 512, outputfp);
+            }
             }
 
         }
