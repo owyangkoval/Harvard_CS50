9972c328cfa80d2269f53dc6466a27482697c231
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 18:44:21 +0000

    Thu, Apr 28, 2022, 6:44 PM +00:00

--- mikechallenge/mikechallenge.c
+++ mikechallenge/mikechallenge.c
@@ -5,9 +5,10 @@ int main(int argc, char *argv[])
 {
 
     FILE *inputfp;
-    int buffer[5] = {0x54, 0x72, 0x75, 0x6d, 0x70};
-    int filename[5];
-    int i=0;
+    FILE *outputfp;
+    int buffer[20];
+    int replacementname[20];
+    int i = 0;
 
     // open 2016.txt
     inputfp = fopen("2016.txt", "r");
@@ -18,17 +19,18 @@ int main(int argc, char *argv[])
     }
 
     // read 2016.txt
-    while (fread(buffer, 1, 5, inputfp) == 5)
-        {
-            printf("%d", *buffer);
-            i++;
-//            sprintf(filename, "Biden");
-//           outputfp = fopen("2020.txt", "w");
-//            if (outputfp == NULL)
-//            {
-//                printf("error\n");
-//                exit(1);
-//            }
-//            fwrite(&buffer, 1, 512, outputfp);
-        }
+    while (fread(buffer, 1, 20, inputfp) == 20)
+    {
+        if ()
+        printf("%d", *buffer);
+        i++;
+        //            sprintf(replacementname, "Biden");
+        //           outputfp = fopen("2020.txt", "w");
+        //            if (outputfp == NULL)
+        //            {
+        //                printf("error\n");
+        //                exit(1);
+        //            }
+        //            fwrite(&buffer, 1, 512, outputfp);
+    }
 }
