ff64dc065f4489d64e32ee8c2a1714af357920be
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-04-28 17:46:34 +0000

    Thu, Apr 28, 2022, 5:46 PM +00:00

--- Mike's Challenge/mikechallenge.c
+++ Mike's Challenge/mikechallenge.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(int argc, char*argv[]){
+
+FILE *intputfp
+
+       //open 2016.txt
+       inputfp = openfp("2016.txt","r");
+        if (inputfp = NULL) {
+            printf("error\n");
+            exit(1);
+        }
+
+        //read 2016.txt
+        in
+
+}
