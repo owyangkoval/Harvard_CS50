840e80e7968fa123cf942e5a1b75098c7aeb13ef
owyangkoval <83048502+owyangkoval@users.noreply.github.com>
2022-06-09 17:44:42 +0000

    Thu, Jun 9, 2022, 5:44 PM +00:00

--- sudoku.c
+++ sudoku.c
@@ -7,17 +7,16 @@ int main(void) {
 int i=0;
 int j=0;
 int rowcolval;
-int sudoku[][];
+int** sudoku = NULL;
 
 //enter number for how many rows/columns needed
 printf("Enter row/column value: ");
-scanf("%d," rowcolval)
+scanf("%d", &rowcolval);
 
 //loop for i j values;
 for (i=0;i<rowcolval;i++) {
     for (j=0;j<rowcolval;j++) {
         scanf("%d", &sudoku[i][j]);
-            printf("%d", sudoku[i][j]);
         }
     }
 }
